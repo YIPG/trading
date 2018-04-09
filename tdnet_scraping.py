2c24360bd94c6d909088ac2713ef7743c21a1974
YIPG <around.mr.child@gmail.com>
2018-04-09 22:37:07 +0900

    決算情報だけ抜き出せたいえーい！

--- tdnet_scraping.py
+++ tdnet_scraping.py
@@ -4,7 +4,7 @@ import feedparser
 import zipfile
 import xml.etree.ElementTree as ET
 
-atom_URL="http://resource.ufocatch.com/atom/tdnetx"
-r=requests.get(atom_URL)
-soup=BeautifulSoup(r.content, "xml")
-print(soup)
+atom_URL = "http://resource.ufocatch.com/atom/tdnetx"
+r = requests.get(atom_URL)
+soup = BeautifulSoup(r.content, "xml")
+print(soup.entry.find(type="text/html").prettify())
