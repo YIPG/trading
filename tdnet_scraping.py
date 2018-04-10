290b00982046c8163c1c6c56c2e07d40e0174098
YIPG <around.mr.child@gmail.com>
2018-04-10 17:07:27 +0900

    挫折した

--- tdnet_scraping.py
+++ tdnet_scraping.py
@@ -7,4 +7,17 @@ import xml.etree.ElementTree as ET
 atom_URL = "http://resource.ufocatch.com/atom/tdnetx"
 r = requests.get(atom_URL)
 soup = BeautifulSoup(r.content, "xml")
-print(soup.entry.find(type="text/html").prettify())
+
+Comp_name_l = soup.find_all("title")
+XBRL_URL_l = soup.find_all(type="text/html")
+for (title, link) in zip(Comp_name_l, XBRL_URL_l):
+    comp = title.string
+    XBRL_URL = link.get("href")
+    # r2 = requests.get(XBRL_URL)
+    # soup2 = BeautifulSoup(r2.content, "html")
+    print(comp,XBRL_URL)
+
+### 挫折した。HTMLをスクレイピングしてやってもいいが劇的に速度が落ちる
+### そもそもAtomAPIの使い方知ってればこんなことには・・・
+### というかキャッシュフローとかみてもSloan Ratioに着目するなら、それだけでいいやんってなった
+### データたくさんとりたいときにこれは必要！！！
